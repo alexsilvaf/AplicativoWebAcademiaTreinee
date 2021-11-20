bd4559719d23fb6b057a157667a986ef4d5e98ca
Alex Ferreira <alex.120999@outlook.com.br>
2021-11-20 15:22:32 -0300

    Atualização de máscara de formatação. - CPF, Telefone e CNPJ

---
+++ AplicativoWebAcademiaTreinee/Controllers/PessoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplicativoWebAcademiaTreinee.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AplicativoWebAcademiaTreinee/wwwroot/js/empresa.js
+++ AplicativoWebAcademiaTreinee/wwwroot/js/empresa.js
@@ -18,28 +18,59 @@
         })
 })()
 
-function MascaraCNPJ() {
-    var cnpj = document.getElementById('validationCustomCnpj')
-    var txtCnpj = cnpj.value
+function mascaraDeCnpj(cnpj) {
+    const textoAtual = cnpj.value;
+    let textoAjustado = textoAtual.replace(/[a-z, A-Z, !-\/]/g, '');
 
-    if (txtCnpj.length == 2) {
-        cnpj.value = txtCnpj + "."
-    }
-    if (txtCnpj.length == 6) {
-        cnpj.value = txtCnpj + "."
-    }
-    if (txtCnpj.length == 10) {
-        cnpj.value = txtCnpj + "/"
-    }
-    if (txtCnpj.length == 15) {
-        cnpj.value = txtCnpj + "-"
-    }
+    const parte1 = textoAjustado.slice(0, 2);
+    const parte2 = textoAjustado.slice(2, 5);
+    const parte3 = textoAjustado.slice(5, 8);
+    const parte4 = textoAjustado.slice(8, 12);
+    const parte5 = textoAjustado.slice(12, 14);
+
+    if (textoAtual.length > 2)
+        textoAjustado = `${parte1}.${parte2}`
+    if (textoAtual.length > 5)
+        textoAjustado = `${parte1}.${parte2}.${parte3}`
+    if (textoAtual.length > 8)
+        textoAjustado = `${parte1}.${parte2}.${parte3}/${parte4}`
+    if (textoAtual.length > 12)
+        textoAjustado = `${parte1}.${parte2}.${parte3}/${parte4}-${parte5}`
+
+    cnpj.value = textoAjustado;
 }
 
-function MascaraCEP() {
-    var cep = document.getElementById('validationCustomCep')
-    txtCep = cep.value
-    if (txtCep.length == 5) {
-        cep.value = txtCep + "-"
-    }
+function mascaraDeCpf(cpf) {
+    const textoAtual = cpf.value;
+    let textoAjustado = textoAtual.replace(/[a-z, A-Z, !-\/]/g, '');
+
+    const parte1 = textoAjustado.slice(0, 3);
+    const parte2 = textoAjustado.slice(3, 6);
+    const parte3 = textoAjustado.slice(6, 9);
+    const parte4 = textoAjustado.slice(9, 11);
+
+    if (textoAtual.length > 3)
+        textoAjustado = `${parte1}.${parte2}`
+    if (textoAtual.length > 6)
+        textoAjustado = `${parte1}.${parte2}.${parte3}`
+    if (textoAtual.length > 9)
+        textoAjustado = `${parte1}.${parte2}.${parte3}-${parte4}`
+
+    cpf.value = textoAjustado;
+}
+
+function filtraNumero(texto) {
+    const conteudo = texto.value;
+    let conteudoAjustado;
+
+    conteudoAjustado = conteudo.replace(/[a-z, A-Z, !-\/]/g, '')
+    texto.value = conteudoAjustado
+}
+
+function filtraLetra(texto) {
+    const conteudo = texto.value;
+    let conteudoAjustado;
+
+    conteudoAjustado = conteudo.replace(/[0-9]/g, '')
+    texto.value = conteudoAjustado
 }
