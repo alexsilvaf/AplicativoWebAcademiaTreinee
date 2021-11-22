ac6e8190f12595c51f04f527698b55e08dea8af6
Alex Ferreira <alex.120999@outlook.com.br>
2021-11-22 17:22:11 -0300

    Atualização do modelo empresa. Adição do construtor

--- AplicativoWebAcademiaTreinee/Models/EmpresaModel.cs
+++ AplicativoWebAcademiaTreinee/Models/EmpresaModel.cs
@@ -4,9 +4,15 @@
     {
         public int Codigo { get; set; }
         public string Nome { get; set; }
-        public int NomeFantasia { get; set; }
+        public string NomeFantasia { get; set; }
         public long CNPJ { get; set; }
 
-        
+        public EmpresaModel(int Codigo, string Nome, string NomeFantasia, long CNPJ)
+        {
+            this.Codigo = Codigo;
+            this.Nome = Nome;
+            this.NomeFantasia = NomeFantasia;
+            this.CNPJ = CNPJ;
+        }
     }
 }
