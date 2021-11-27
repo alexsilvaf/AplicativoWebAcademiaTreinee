2819a03f209b6d189f551f5264a866a240e3e05c
Alex Ferreira <alex.120999@outlook.com.br>
2021-11-27 19:54:27 -0300

    Atualizando o nome das tabelas e inserindo botão de switch.

--- AplicativoWebAcademiaTreinee/Models/PessoaModel.cs
+++ AplicativoWebAcademiaTreinee/Models/PessoaModel.cs
@@ -6,11 +6,15 @@ namespace AplicativoWebAcademiaTreinee.Models
     {
         [Key]
         public int Codigo { get; set; }
+        [Display(Name = "Situação")]
         public string? Situacao { get; set; }
         public string? Nome { get; set; }
         public string? Email { get; set; }
+        [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
+        [Display(Name = "Quantidade de Filhos")]
         public int QuantidadeFilhos { get; set; }
+        [Display(Name = "Salário")]
         public int Salario { get; set; }
     }
 }
